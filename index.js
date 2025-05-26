b415121327dfcf89d2885ffc7149a22b62ee7be5
David Termux <davidffsousaff@gmeil.com>
2025-05-26 00:40:12 -0300

    Add export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export function somar(a, b) {
+export default function somar(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new Error('Os parâmetros precisam ser números');
   }
